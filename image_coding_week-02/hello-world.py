ac970de658f28540a1f21897560773b7ec99497b
PanaddaK <kwinging@gmail.com>
2025-12-02 15:24:18 +0700

    1

---
+++ image_coding_week-02/hello-world.py
@@ -0,0 +1,12 @@
+# hello_world.py
+
+# Define the message to be displayed.
+message = "Hello, World! From Image Processing Class"
+
+# Display the message to the console.
+print(message)
+
+# Additional information: Display student name and ID (for code documentation).
+print("--------------------------------------------------")
+print("Prepared by: [Student Name]") 
+print("Student ID: [Student ID]")
